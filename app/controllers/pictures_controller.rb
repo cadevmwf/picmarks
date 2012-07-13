eae90a33b3252c0419e1c7ca5004a9a6f69fad7f
Jeff Cohen <jeff@codeacademy.org>
2012-07-13 10:51:16 -0500

    Start working with models

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,19 +1,28 @@
 class PicturesController < ApplicationController
   def all_pictures
-    return [
-      "http://www.w3.org/html/logo/downloads/HTML5_Logo_512.png",
-      "http://farm4.staticflickr.com/3419/3202272106_86e6c38fc8_z.jpg?zz=1",
-      "http://www.bugtreat.com/blog/wp-content/uploads/2012/06/ruby-on-rails-logo.png"
-      ] 
+    
+    pics = []
+    
+    ["http://www.w3.org/html/logo/downloads/HTML5_Logo_512.png",
+      "http://www.bugtreat.com/blog/wp-content/uploads/2012/06/ruby-on-rails-logo.png"].each do |url|
+        p = Picture.new
+        p.url = url
+        pics.push(p)
+    end
+        
+    return pics
   end
   
+  def new
+    
+  end
   def index
     @pictures = all_pictures
   end
   
   def show
     pic_id = params["id"].to_i
-    @url = all_pictures[pic_id]
+    @picture = all_pictures[pic_id]
     @bgcolor = params["color"]
   end
   
---
+++ app/models/picture.rb
@@ -0,0 +1,5 @@
+class Picture
+  attr_accessor :url
+  attr_accessor :note
+  attr_accessor :favorite
+end
